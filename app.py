8f5701f6a6991520e3c7a4aca25362ba40dcaf29
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-12-27 03:18:14 +0600

    commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # flask app url routing
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
+# render_template is used when there is get request
+# request is used  
 
 app=Flask(__name__)
 
@@ -13,6 +15,34 @@ def welcome():
 def index():
     return "it is index page"
 
+@app.route("/success/<int:score>")
+def success(score):
+    return "the person has passes with score"+str(score)
 
+@app.route("/fail/<float:score>")
+def fail(score):
+    return "The person has failed with score " + str(score)
+
+@app.route('/form',methods=["GET","POST"])
+def form():
+    if request.method=='GET':
+        return render_template('form.html')
+    # this form.html file should be under templates folder
+    else:
+        maths=float(request.form['maths'])
+        science=float(request.form['science'])
+        history=float(request.form['history'])
+        
+        average_marks=(maths+science+history)/3
+        res=""
+        if average_marks>50:
+            res="success"
+        else:
+            res="fail"
+        return  redirect(url_for(res,score=average_marks))
+        
+        # return render_template('form.html',score=average_marks)
+
+    
 if __name__=="__main__":
     app.run(debug=True)
